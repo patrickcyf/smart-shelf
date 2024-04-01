4173ac2c5ca64b24472d4403382ffb1b4b79ccee
Patrick Cheung <patrickcyf@gmail.com>
2024-04-01 09:56:41 +0800

    display video capture

--- predict.py
+++ predict.py
@@ -1,5 +1,19 @@
+import cv2
+
 def main():
-	print("hello")
+	cap = cv2.VideoCapture('video/overpass.mp4')
+
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		if ret == True:
+			cv2.imshow('Frame',frame)
+			if cv2.waitKey(25) & 0xFF == ord('q'):
+				break
+		else:
+			break
+	
+	cap.release()
+	cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 	main()
