d072439547b02e8f74430d605d1458f0535a0ca0
Patrick Cheung <patrickcyf@gmail.com>
2024-04-07 10:36:16 +0800

    fix formating issue in predict.py

--- predict.py
+++ predict.py
@@ -1,19 +1,21 @@
 import cv2
 
-def main():
-	cap = cv2.VideoCapture('video/overpass.mp4')
 
-	while(cap.isOpened()):
-		ret, frame = cap.read()
-		if ret == True:
-			cv2.imshow('Frame',frame)
-			if cv2.waitKey(25) & 0xFF == ord('q'):
-				break
-		else:
-			break
-	
-	cap.release()
-	cv2.destroyAllWindows()
+def main():
+    cap = cv2.VideoCapture("video/overpass.mp4")
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-	main()
+    main()
