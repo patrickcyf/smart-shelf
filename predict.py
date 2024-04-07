a619c82af49776fbd0df64b2143f2104e90e5692
Patrick Cheung <patrickcyf@gmail.com>
2024-04-07 12:07:20 +0800

    resize item detection windows

--- predict.py
+++ predict.py
@@ -3,11 +3,12 @@ import cv2
 
 def main():
     cap = cv2.VideoCapture("video/example.mp4")
-
+    cv2.namedWindow("item detection", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("item detection", 800, 600)
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is True:
-            cv2.imshow("Frame", frame)
+            cv2.imshow("item detection", frame)
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 break
         else:
