f85a17fc65ce345aae8b9085a337c6c2e3939d4f
Patrick Cheung <patrickcyf@gmail.com>
2024-06-29 18:16:29 +0800

    rename video folder to videos

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import cv2
 
 
 def main():
-    cap = cv2.VideoCapture("video/example.mp4")
+    cap = cv2.VideoCapture("videos/example.mp4")
     cv2.namedWindow("item detection", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("item detection", 800, 600)
     while cap.isOpened():
